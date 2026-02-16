0253c2bd354876b158ee705fafcd21101ce4f289
AdolfoNalin <nalinadolfo33@gmail.com>
2026-02-16 19:45:57 -0300

    Criando a Classe transaction

---
+++ AppFinancialControl/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFinancialControl.Models
+{
+    public enum TransactionType
+    { 
+        Income,
+        Expenses
+    }
+
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TransactionType Type { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public float Value { get; set; }
+    }
+}
