881303d7464745654311ed791d0de61dff9288bf
AdolfoNalin <nalinadolfo33@gmail.com>
2026-02-17 20:11:43 -0300

    Classe responsavél por capiturar e montar o caminho do banco de dados

---
+++ AppFinancialControl/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFinancialControl
+{
+    public class AppSettings
+    {
+        private static string DataBaseName = "database.db";
+        private static string DataBaseDirectory = FileSystem.AppDataDirectory;
+        public static string DataBasePath = Path.Combine(DataBaseDirectory, DataBaseName);
+    }
+}
