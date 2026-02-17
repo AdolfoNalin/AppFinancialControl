f30162bfc6cc5c3b36e9540c871ee85affe1a96e
AdolfoNalin <nalinadolfo33@gmail.com>
2026-02-17 17:34:15 -0300

    criando esqueleto da Classe TransactionService

---
+++ AppFinancialControl/Service/ITransactionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using System.Transactions;
+
+namespace AppFinancialControl.Service
+{
+    interface ITransactionService
+    {
+        public List<Transaction> GetAll();
+        public void Add(Transaction transaction);
+        public void Update(Transaction transaction);
+        public void Delete(Transaction transaction);
+    }
+}
---
+++ AppFinancialControl/Service/TransactionService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace AppFinancialControl.Service
+{
+    public class TransactionService : ITransactionService   
+    {
+        #region GetAll
+        public List<Transaction> GetAll()
+        {
+            try
+            {
+                return new List<Transaction>();
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+        #endregion
+
+        #region Add
+        public void Add(Transaction transaction)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Update
+        public void Update(Transaction transaction)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region Delete
+        public void Delete(Transaction transaction)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+    }
+}
