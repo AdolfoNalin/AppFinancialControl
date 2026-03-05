eddc19fe3a7d53d268eee5631ad2c9d8b9f6cda0
AdolfoNalin <nalinadolfo33@gmail.com>
2026-03-05 20:18:51 -0300

    Desenvolvendo a metodo button_Clicked

--- AppFinancialControl/View/TransactionAdd.xaml.cs
+++ AppFinancialControl/View/TransactionAdd.xaml.cs
@@ -35,7 +35,8 @@ public partial class TransactionAdd : ContentPage
             {
                 Name = EntryName.Text ?? throw new ArgumentNullException("Nome é um campo necessário"),
                 Date = DPDate.Date ?? throw new ArgumentNullException("Data é um campo necessário"),
-                Value = float.Parse(EntryValue.Text ?? throw new ArgumentNullException("Valor é um campo necessário")),
+                Value = float.Parse(EntryValue.Text ?? throw new ArgumentNullException("Valor é um campo necessário")
+                ?? throw new FormatException("O campo 'Valor' só pode ser digitado somente números")),
                 Type = rbEntry.IsChecked == true ? TransactionType.Income : TransactionType.Expenses,
             };
 
@@ -45,6 +46,10 @@ public partial class TransactionAdd : ContentPage
             DisplayAlert("Aceito", "Transição salva", "ok");
 
             Navigation.PopModalAsync();
+        }
+        catch(FormatException fe)
+        {
+            DisplayAlert("Error", $"{fe.Message}", "OK");
         }
         catch(ArgumentNullException ane)
         {
