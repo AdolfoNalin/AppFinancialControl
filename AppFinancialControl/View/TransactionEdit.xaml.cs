ee0aa677baaadcbca30d395190935549ba8a8f8d
AdolfoNalin <nalinadolfo33@gmail.com>
2026-03-10 20:38:23 -0300

    Programando a tela TransactionEdit

--- AppFinancialControl/View/TransactionEdit.xaml.cs
+++ AppFinancialControl/View/TransactionEdit.xaml.cs
@@ -1,16 +1,19 @@
 
 
 using AppFinancialControl.Models;
+using AppFinancialControl.Service;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace AppFinancialControl.View;
 
 public partial class TransactionEdit : ContentPage
 {
     private Transaction _transaction;
-	public TransactionEdit()
+    private ITransactionService _service;
+	public TransactionEdit(ITransactionService service)
 	{
 		InitializeComponent();
-
+        _service = service;
 	}
 
     public void SetTransactionToEdit(Transaction transaction)
@@ -48,6 +51,39 @@ public partial class TransactionEdit : ContentPage
         catch (Exception ex)
         {
             await DisplayAlert("Erro", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "OK");
+        }
+    }
+    #endregion
+
+    #region SaveEdit
+    private void SaveEdit(object sender, EventArgs e)
+    {
+        try
+        {
+            Transaction transaction = new Transaction()
+            {
+                Id = _transaction.Id,
+                Name = etName.Text ?? throw new ArgumentNullException("Nome é um campo necessário"),
+                Type = rbExpense.IsChecked == true ? TransactionType.Expenses : TransactionType.Income,
+                Date = dpDate.Date ?? throw new ArgumentNullException("Data é um campo necessário"),
+                Value = float.Parse(etValue.Text.Replace("R$", "") ?? throw new ArgumentNullException("Valor é um campo necessário")),
+            };
+
+            _service.Update(transaction);
+
+            DisplayAlert("Aceito", "Trasação atualizada", "Ok");
+
+            Navigation.PopModalAsync();
+
+            WeakReferenceMessenger.Default.Send<string>("");
+        }
+        catch(ArgumentNullException ane)
+        {
+            DisplayAlert("Error", $"{ane.ParamName}", "Ok");
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Error", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "OK");
         }
     }
     #endregion
