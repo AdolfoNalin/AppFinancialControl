b907cae630ca237cdbf220c400c047d548bd388c
AdolfoNalin <nalinadolfo33@gmail.com>
2026-03-10 21:15:59 -0300

    Mudando a cor dos valores da tela transactionList
    Se for despesa é vermelho se for receita é verde

---
+++ AppFinancialControl/Libraries/Converters/TransactionValueColorConvert.cs
@@ -0,0 +1,44 @@
+﻿using AppFinancialControl.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AppFinancialControl.Libraries.Converters
+{
+    public class TransactionValueColorConvert : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            try
+            {
+                Transaction transaction = (Transaction)value
+                    ?? throw new ArgumentNullException();
+
+                if (transaction.Type == TransactionType.Income)
+                {
+                    return Colors.Green;
+                }
+                else
+                {
+                    return Colors.Red;
+                }
+            }
+            catch(ArgumentNullException ane)
+            {
+                return null;
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+           
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppFinancialControl/Libraries/Converters/TransactionValueConverter.cs
@@ -0,0 +1,45 @@
+﻿using AppFinancialControl.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AppFinancialControl.Libraries.Converters
+{
+    public class TransactionValueConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            try
+            {
+
+                Transaction transaction = (Transaction)value
+                ?? throw new ArgumentNullException();
+
+                if (transaction.Type == TransactionType.Income)
+                {
+                    return transaction.Value.ToString("C");
+                }
+                else
+                {
+                    return $"-{transaction.Value.ToString("C")}";
+                }
+            }
+            catch(ArgumentNullException ane)
+            {
+                return null;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+           
+        }
+
+        public object? ConvertBack(object value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
