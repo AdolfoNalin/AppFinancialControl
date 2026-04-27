f13ca2dc211b5164e9226d83683bd863e7b9dc1e
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-04-26 21:38:35 -0300

    Criando o objeto Usuário

---
+++ AppFinancialControl/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace AppFinancialControl.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
