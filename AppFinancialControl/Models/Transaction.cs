4015bf2acf3805c5c3a452c56dea14dc37bbd58b
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-04-26 21:44:59 -0300

    Tirando os using que não está em uso

--- AppFinancialControl/Models/Transaction.cs
+++ AppFinancialControl/Models/Transaction.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AppFinancialControl.Models
+﻿namespace AppFinancialControl.Models
 {
     public enum TransactionType
     { 
