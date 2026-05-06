bc15c5d9cec703b2934f08ba059cdea5578dba35
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-05-05 21:33:34 -0300

    Adicionando os obejtos userLogin e userSession

---
+++ AppFinancialControl/Models/UserSession.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFinancialControl.Models
+{
+    public class UserSession 
+    {
+        public static int Id { get; set; }
+        public static string Login { get; set; }
+        public static string Password { get; set; }
+    }
+}
