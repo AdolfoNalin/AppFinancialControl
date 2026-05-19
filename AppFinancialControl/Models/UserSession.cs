c1c84d3cac19e6ddf1a6dd7e0da5af7b59521e9c
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-05-19 20:36:24 -0300

    Add the attribute Token

--- AppFinancialControl/Models/UserSession.cs
+++ AppFinancialControl/Models/UserSession.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AppFinancialControl.Models
+﻿namespace AppFinancialControl.Models
 {
     public class UserSession 
     {
         public static int Id { get; set; }
         public static string Login { get; set; }
         public static string Password { get; set; }
+        public static string Token { get; set; }
     }
 }
