dd16ec8566833fff1307fe5883d492527688801c
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-05-19 21:23:47 -0300

    Switch parameter of method Login

--- AppFinancialControl/Service/IUserService.cs
+++ AppFinancialControl/Service/IUserService.cs
@@ -1,14 +1,10 @@
 ﻿using AppFinancialControl.Models;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
 
 namespace AppFinancialControl.Service
 {
     public interface IUserService
     {
-        public bool Login(User user);
+        public User Login(UserLogin user);
         public void Insert(User user);
         public void Delete(User user);
         public void Update(User user);
