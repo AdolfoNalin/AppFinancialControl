906c161c2fde9acbc8be7d2e070bafbe6955f460
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-05-23 12:52:16 -0300

    Create class client

---
+++ AppFinancialControl/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFinancialControl.Models
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
