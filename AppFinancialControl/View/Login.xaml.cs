9d7e148b0ed3ff22aee7ff38b7d40e5d533a8d18
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-05-27 21:06:05 -0300

    Programming the screen Login

--- AppFinancialControl/View/Login.xaml.cs
+++ AppFinancialControl/View/Login.xaml.cs
@@ -1,30 +1,154 @@
+using AppFinancialControl.Models;
+using AppFinancialControl.Service;
+
 namespace AppFinancialControl.View;
 
 public partial class Login : ContentPage
 {
-	public Login()
+    IUserService _service;
+
+	public Login(IUserService userService)
 	{
 		InitializeComponent();
+        _service = userService;
 	}
-
-    #region Button_Clicked_ReadThePassword
-    private void Button_Clicked_ReadThePassword(object sender, EventArgs e)
+    #region Button_clicked_Login
+    /// <summary>
+    /// Button event Login
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void Button_Clicked_Login(object sender, EventArgs e)
     {
         try
         {
-            if(txtPassoword.IsPassword)
+            UserLogin login = new UserLogin()
             {
-                txtPassoword.IsPassword = false;
+                Login = txtEmail.Text ?? 
+                throw new ArgumentNullException("Email ou nome do usuário é obrigatorio"),
+                Password = txtPassoword.Text ?? 
+                throw new ArgumentNullException("Senha é obrigatorio"),
+            };
+
+            User user = await _service.Login(login);
+
+            if (user != null)
+            {
+                UserSession.Id = user.Id;
+                UserSession.Login = user.Login;
+                UserSession.Password = user.Password;
+                UserSession.Token = user.Token;
+
+                TransactionList list = this.Handler.MauiContext.Services.GetService<TransactionList>()
+                    ?? throw new ArgumentNullException("MAUI Context");
+
+                Navigation.PushAsync(list);
             }
             else
             {
-                txtPassoword.IsPassword = true;
+                throw new ArgumentNullException("Usuário não encontrado");
             }
+        }
+        catch(NullReferenceException nre)
+        {
+            DisplayAlert("Erro", nre.Message, "Fechar");
+        }
+        catch(ArgumentNullException ane)
+        {
+            DisplayAlert("Erro",ane.ParamName, "Fechar");
+        }
+
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+    #endregion
+
+    #region ImageButton_Clicked_IsSeePassword
+    /// <summary>
+    /// Método responsável mudar a imagem e o tipo de caracteri do txtPassword
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ImageButton_Clicked_IsSeePassword(object sender, EventArgs e)
+    {
+        if(txtPassoword.IsPassword)
+        {
+            txtPassoword.IsPassword = false;
+            btnImageSee.Source = "olho.png";
+        }
+        else
+        {
+            txtPassoword.IsPassword = true;
+            btnImageSee.Source = "olho_fechado.png";
+        }
+    }
+    #endregion
+
+    #region ImageButton_Clicked_EntryGoogle
+    private async void Button_Clicked_EntryGoogle(object sender, EventArgs e)
+    {
+        try
+        {
+            
         }
         catch (Exception ex)
         {
             DisplayAlert("Erro", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "Fechar");
         }
+    }
+    #endregion
+
+    #region ImageButton_Clicked_EntryGithub
+    private void Button_Clicked_EntryGithub(object sender, EventArgs e)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+            DisplayAlert("Erro", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "Fechar");
+        }
+    }
+    #endregion
+
+    #region ImageButton_Clicked_EntryFacebook
+    private void Button_Clicked_EntryFacebook(object sender, EventArgs e)
+    {
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+            DisplayAlert("Erro", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "Fechar");
+        }
+    }
+    #endregion
+
+    #region Button_Clicked_EntryForApp
+    private void Button_Clicked_EntryForApp(object sender, EventArgs e)
+    {
+        try
+        {
+            InsertClient client = this.Handler.MauiContext.Services.GetService<InsertClient>();
+            Navigation.PushAsync(client);
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Error", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "Fechar");
+        }
+    }
+    #endregion
+
+    #region Clicked_Enter_Button
+    private void Clicked_Enter_Button(object sender, EventArgs e)
+    {
+        Button_Clicked_Login(sender, e);
     }
     #endregion
 }
