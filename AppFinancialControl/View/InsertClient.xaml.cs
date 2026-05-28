5c31cdcca37fc61b7c6a35ed05bb62460a32fd98
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-05-27 21:14:06 -0300

    Cliente the Screen responsable for insert Client in database

---
+++ AppFinancialControl/View/InsertClient.xaml.cs
@@ -0,0 +1,45 @@
+using AppFinancialControl.Models;
+using AppFinancialControl.Service;
+
+namespace AppFinancialControl.View;
+
+public partial class InsertClient : ContentPage
+{
+    private readonly IClientService _service;
+	public InsertClient(IClientService service)
+	{
+        _service = service;
+		InitializeComponent();
+	}
+
+    private void Button_Clicked_Cancel(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+
+    #region Button_Clicked_Next
+    private async void Button_Clicked_Next(object sender, EventArgs e)
+    {
+        try
+        {
+            Client client = new Client()
+            {
+                Name = txtNameClient.Text ?? throw new ArgumentNullException("Nome é obrigatório"),
+                Email = txtEmailClient.Text ?? throw new ArgumentNullException("Email é obrigatório"),
+                Salary = Decimal.Parse(txtSalaryClient.Text)
+            };
+
+            string message = await _service.Add(client);
+
+            DisplayAlert("Deu certo", message, "Fechar");
+
+            InsertUser insertUser = this.Handler.MauiContext.Services.GetService<InsertUser>();
+            Navigation.PushAsync(insertUser);
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Erro", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}, {ex.InnerException.Message}", "Fechar");
+        }
+    }
+    #endregion
+}
