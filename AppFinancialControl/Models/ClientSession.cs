557711d06a8b8f392735a48c25b70f9f0e17ad48
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-05-27 21:10:48 -0300

    Create the class for capture Id Client

---
+++ AppFinancialControl/Models/ClientSession.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFinancialControl.Models
+{
+    public class ClientSession
+    {
+        public static Guid Id { get; set; }
+    }
+}
