4d3333ddbdf4f74f0cca4e6213e5de24540ad84d
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-06-04 07:36:49 -0300

    Create the AppShell
    Static menu

---
+++ AppFinancialControl/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+using AppFinancialControl.View;
+
+namespace AppFinancialControl;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+        InitializeComponent();
+
+        Dispatcher.Dispatch(async () =>
+        {
+            await Shell.Current.GoToAsync("//home");
+        });
+    }
+}
