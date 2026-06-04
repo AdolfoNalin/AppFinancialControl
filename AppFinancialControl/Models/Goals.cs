6e148138fbfb7b1a2506008d77173e9491788925
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-06-04 07:50:08 -0300

    Create the class Goals

---
+++ AppFinancialControl/Models/Goals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFinancialControl.Models
+{
+    public class Goals
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
