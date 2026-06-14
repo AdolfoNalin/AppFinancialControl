219639c47679ffa264e157f7b4a8ebed44e3c9c1
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-06-13 21:24:09 -0300

    Class that configures the border data Summary

---
+++ AppFinancialControl/Models/Summary.cs
@@ -0,0 +1,38 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+public class Summary
+{
+    public static ISeries[] Series { get; set; } =
+    [
+        new PieSeries<double>
+        {
+            Values = [10],
+            Name = "Alimentação"
+        },
+
+        new PieSeries<double>
+        {
+            Values = [25],
+            Name = "Transporte"
+        },
+
+        new PieSeries<double>
+        {
+            Values = [40],
+            Name = "Lazer"
+        }
+    ];
+
+    public static ISeries[] GetSeries()
+    {
+        try
+        {
+            return Series;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+}
