4ef2296149a4af842a16f93be06ebdf69841b4c4
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-06-13 21:26:19 -0300

    Create the Class ExtractList
    Class responsable for List all Transaction order by Date

---
+++ AppFinancialControl/View/ExtractList.xaml.cs
@@ -0,0 +1,59 @@
+using AppFinancialControl.Models;
+using AppFinancialControl.Service;
+using CommunityToolkit.Mvvm.Messaging;
+using System.Collections.ObjectModel;
+
+namespace AppFinancialControl.View;
+
+public partial class ExtractList : ContentPage
+{
+    private readonly ITransactionService _service;
+	public ExtractList(ITransactionService servuce)
+	{
+        _service = servuce;
+		InitializeComponent();
+
+        UpdateData();
+        WeakReferenceMessenger.Default.Register<string>(this, (e, message) =>
+        {
+            UpdateData();
+        });
+	}
+
+    #region UpateData
+    private void UpdateData()
+    {
+        try
+        {
+            DateOnly dateNow = DateOnly.Parse(DateTime.Now.Date.Date.ToString("D"));
+
+            ObservableCollection<Transaction> transactions = _service.GetDate(UserSession.Id, dateNow, dateNow)
+                ?? throw new NullReferenceException("Nenhuma transação realizada neste perildo de tempo");
+
+
+            float income = transactions.Where(b => b.Type == TransactionType.Income)
+                .Sum(a => a.Value);
+
+            float expensse = transactions.Where(b => b.Type == TransactionType.Expenses)
+               .Sum(a => a.Value);
+
+            float balance = income - expensse;
+
+            lblBalance.Text = balance.ToString("C");
+            lblIncome.Text = income.ToString("C");
+            lblExpense.Text = expensse.ToString("C");
+
+            cvExtract.BindingContext = transactions;
+        }
+        catch(NullReferenceException nre)
+        {
+            DisplayAlert("Erro", nre.Message, "Fechar");
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Erro", ex.InnerException.Message, "Fechar");
+        }
+    }
+    #endregion
+
+}
