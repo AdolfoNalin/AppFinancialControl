17cdda81914cbd9a6ef8281847eb8a3a4697c850
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-06-13 21:20:30 -0300

    Object create for organize the Display graphic

---
+++ AppFinancialControl/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppFinancialControl.Models
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+    }
+}
