4c3d06105705579f37f7f8d0354651a705c5c5dc
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-06-30 06:51:05 -0300

    Create Class responsable for Converter the title goals

---
+++ AppFinancialControl/Libraries/Converters/GoalsNameConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace AppFinancialControl.Libraries.Converters
+{
+    class GoalsNameConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            try
+            {
+                string result = (String)value ??
+                  throw new ArgumentNullException("O campo nome não foi preenchido");
+
+                return result.ToUpper().First();
+            }
+            catch (ArgumentNullException ane)
+            {
+                throw ane;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
