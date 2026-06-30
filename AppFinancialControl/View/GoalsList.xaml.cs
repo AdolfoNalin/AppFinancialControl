62d6109d8a697af8094c1af7c0385494809e869d
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-06-30 07:01:32 -0300

    Organize the screen Goals

--- AppFinancialControl/View/GoalsList.xaml.cs
+++ AppFinancialControl/View/GoalsList.xaml.cs
@@ -26,7 +26,8 @@ public partial class GoalsList : ContentPage
     {
         try
         {
-
+            GoalsAdd add = this.Handler.MauiContext.Services.GetService<GoalsAdd>();
+            Navigation.PushAsync(add);
         }
         catch (Exception ex)
         {
@@ -48,6 +49,46 @@ public partial class GoalsList : ContentPage
         catch (Exception ex)
         {
             DisplayAlert("Erro", ex.InnerException.Message, "Fechar");
+        }
+    }
+    #endregion
+
+    #region Tapped_Delete_Goals
+    private async void Tapped_Delete_Goals(object sender, TappedEventArgs e)
+    {
+        try
+        {
+            bool result = await DisplayAlert("Deletar?", "Deseja excluir essa meta?", "Sim", "Não");
+
+            if (result)
+            {
+                Goals goalsConvert = e.Parameter as Goals;
+                await DisplayAlert("Sucesso", _service.Delete(goalsConvert.Id), "Fechar");
+                WeakReferenceMessenger.Default.Send("");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Erro", $"{ex.StackTrace}, {ex.Message}, {ex.HelpLink}", "Fechar");
+        }
+    }
+    #endregion
+
+    #region Tapped_Update_Goals
+    private void Tapped_Update_Goals(object sender, TappedEventArgs e)
+    {
+        try
+        {
+            Goals goals = e.Parameter as Goals;
+            GoalsEdit screen = this.Handler.MauiContext.Services.GetService<GoalsEdit>();
+            screen.SetGoalsScreenEdit(goals);
+
+            Navigation.PushAsync(screen);
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Erro", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "Fechar");
         }
     }
     #endregion
