bbcc4569f787a58387a7e04787891da7ba95c7f1
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-06-30 06:52:13 -0300

    Create verification Direct login

--- AppFinancialControl/App.xaml.cs
+++ AppFinancialControl/App.xaml.cs
@@ -1,23 +1,68 @@
-﻿using AppFinancialControl.Service;
+﻿using AppFinancialControl.Models;
+using AppFinancialControl.Service;
 using AppFinancialControl.View;
+using System.Runtime.CompilerServices;
 
 namespace AppFinancialControl
 {
     public partial class App : Application
     {
-        IUserService _service;
-        public App(Login login)
+        private User _user = null;
+        private IUserService _service;
+        public App(IUserService service)
         {
-            InitializeComponent();
+            _service = service;
+            try
+            {
+                InitializeComponent();
+                GetUser();
 
-            MainPage = login;
+                if (_user == null)
+                {
+                    Login login = new Login(_service);
+                    MainPage = login;
+                }
+                else
+                {
+                    UserSession.Id = _user.Id;
+                    UserSession.Login = _user.Login;
+                    UserSession.Password = _user.Password;
+                    UserSession.Token = _user.Token;
+
+                    MainPage = new AppShell();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
+        private async void GetUser()
+        {
+            try
+            {
+                Guid id = Guid.Parse(await SecureStorage.Default.GetAsync("userId") ?? 
+                    throw new NullReferenceException());
+                if(id != Guid.Empty)
+                {
+                    _user = _service.GetUser(id);
+                }
+            }
+            catch(NullReferenceException nre)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         protected override Window CreateWindow(IActivationState? activationState)
         {
             var window = base.CreateWindow(activationState);
             window.Height = 700;
-            window.Width = 300;
+            window.Width = 600;
             return window;
 
         }
