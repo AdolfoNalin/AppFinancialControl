217b577a63a54eeb7fc8ee7ff68c5bf05ad9fcb3
Adolfo Nalin <nalinadolfo33@gmai.com>
2026-07-02 21:30:42 -0300

    Finish create the screen Extract

--- AppFinancialControl/View/ExtractList.xaml.cs
+++ AppFinancialControl/View/ExtractList.xaml.cs
@@ -8,28 +8,77 @@ namespace AppFinancialControl.View;
 public partial class ExtractList : ContentPage
 {
     private readonly ITransactionService _service;
+    public ObservableCollection<MonthItem> Months { get; } = new();
 	public ExtractList(ITransactionService servuce)
 	{
         _service = servuce;
 		InitializeComponent();
+
+        AddingDateInCarouseView();
 
         UpdateData();
         WeakReferenceMessenger.Default.Register<string>(this, (e, message) =>
         {
             UpdateData();
         });
-	}
+    }
+
+    private MonthItem _selectedMonth;
+
+    public MonthItem SelectedMonth
+    {
+        get => _selectedMonth;
+        set
+        {
+            if (_selectedMonth != value)
+            {
+                _selectedMonth = value;
+
+                _service.GetDate(UserSession.Id, value);
+
+                OnPropertyChanged(nameof(SelectedMonth));
+            }
+        }
+    }
+
+    #region AddingDateInCarouseView
+    public void AddingDateInCarouseView()
+    {
+        try
+        {
+            var current = DateTime.Today;
+
+            for (int i = -12; i <= 12; i++)
+            {
+                var date = current.AddMonths(i);
+
+                Months.Add(new MonthItem
+                {
+                    Month = date.Month,
+                    Year = date.Year
+                });
+            }
+
+            cvMonths.ItemsSource = Months;
+            cvMonths.Position = 12;
+            cvMonths.CurrentItem = SelectedMonth;
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Erro", $"{ex.Message}, {ex.StackTrace}, {ex.HelpLink}", "Fechar");
+        }
+    }
+    #endregion
 
     #region UpateData
     private void UpdateData()
     {
         try
         {
-            DateOnly dateNow = DateOnly.Parse(DateTime.Now.Date.ToString("D"));
+            MonthItem value = Months[cvMonths.Position];
 
-            ObservableCollection<Transaction> transactions = _service.GetAll(UserSession.Id)
-                ?? throw new NullReferenceException("Nenhuma transação realizada neste perildo de tempo");
-
+            ObservableCollection<Transaction> transactions = _service.GetDate(UserSession.Id, value)
+                ?? throw new NullReferenceException("Nenhum item encontrado");
 
             float income = transactions.Where(b => b.Type == TransactionType.Income)
                 .Sum(a => a.Value);
@@ -56,4 +105,8 @@ public partial class ExtractList : ContentPage
     }
     #endregion
 
+    private void UpdateDataDate(object sender, PositionChangedEventArgs e)
+    {
+        UpdateData();
+    }
 }
